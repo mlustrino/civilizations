4a6c1a73630f4044e9860bde5614694ca5fb69ec
MLS-2004 <marclosada2004@gmail.com>
2026-05-05 18:34:09 +0200

    Interfaz militaryUnit

---
+++ M3/src/militaryUnit/MilitaryUnit.java
@@ -0,0 +1,27 @@
+package militaryUnit;
+
+interface MilitaryUnit{
+	abstract int attack();
+	
+	abstract void takeDamage(int receivedDamage);
+	
+	abstract int getActualArmor();
+	
+	abstract int getFoodCost();
+	
+	abstract int getWoodCost();
+	
+	abstract int getIronCost();
+	
+	abstract int getManaCost();
+		
+	abstract int getChanceGeneratingWaste();
+	
+	abstract int getChanceAttackAgain();
+	
+	abstract void resetArmor();
+	
+	abstract void setExperience(int n);
+	
+	abstract int getExperience();
+}
