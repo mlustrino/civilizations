a82e33c7af6dd93db2885483ff335c1baa3487b5
unaiesvaz <unaiesvaz@gmail.com>
2026-05-05 20:12:11 +0200

    Creacion de clases
    Creacion de las clases DefenseUnit, ArrowTower, Catapult, RocketLauncherTower, SpecialUnit, Magician, Priest

---
+++ M3/src/specialUnit/Magician.java
@@ -0,0 +1,57 @@
+package specialUnit;
+
+class Magician extends SpecialUnit {
+
+	public Magician(int armor, int baseDamage) {
+		super(armor, baseDamage);
+	}
+
+	public int attack() {
+		return 0;
+	}
+
+	public void takeDamage(int receivedDamage) {
+		
+	}
+
+	public int getActualArmor() {
+		return 0;
+	}
+
+	public int getFoodCost() {
+		return 0;
+	}
+
+	public int getWoodCost() {
+		return 0;
+	}
+
+	public int getIronCost() {
+		return 0;
+	}
+
+	public int getManaCost() {
+		return 0;
+	}
+
+	public int getChanceGeneratingWaste() {
+		return 0;
+	}
+
+	public int getChanceAttackAgain() {
+		return 0;
+	}
+
+	public void resetArmor() {
+		
+	}
+
+	public void setExperience(int n) {
+		
+	}
+
+	public int getExperience() {
+		return 0;
+	}
+	
+}
