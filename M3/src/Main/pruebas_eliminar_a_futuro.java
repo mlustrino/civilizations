043a30f7bfae061cbcab0d7384743d86da19e453
unaiesvaz <unaiesvaz@gmail.com>
2026-05-06 18:39:40 +0200

    Cambios y creacion de metodos para las unidades

---
+++ M3/src/Main/pruebas_eliminar_a_futuro.java
@@ -0,0 +1,19 @@
+package Main;
+import Attack.*;
+import battle.*;
+import defenseUnit.*;
+import militaryUnit.*;
+import specialUnit.*;
+import variables.*;
+
+
+public class pruebas_eliminar_a_futuro {
+
+	public static void main(String[] args) {
+		Cannon c1 = new Cannon(20,5);
+		Cannon c2 = new Cannon();
+		ArrowTower at1 = new ArrowTower(20,5);
+
+	}
+
+}
