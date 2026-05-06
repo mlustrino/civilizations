40242a51d0334d9c5280fd64c5e036e3ca9642c3
unaiesvaz <unaiesvaz@gmail.com>
2026-05-06 17:58:14 +0200

    Commit metodos_defense_unit

--- M3/src/defenseUnit/DefenseUnit.java
+++ M3/src/defenseUnit/DefenseUnit.java
@@ -11,14 +11,59 @@ abstract class DefenseUnit implements MilitaryUnit, Variables {
 	private boolean sanctified;
 	
 	public DefenseUnit(int armor, int baseDamage) {
-		super();
-		this.armor = armor;
-		this.baseDamage = baseDamage;
-		this.initialArmor = armor;
-		this.experience = 0;
-		this.sanctified = false;
-		
+	    this.armor = armor;
+	    this.initialArmor = armor;
+	    this.baseDamage = baseDamage;
+	    this.experience = 0;
+	    this.sanctified = false;
 	}
+	
+	public String toString() {
+		return "DefenseUnit [armor=" + armor + ", initialArmor=" + initialArmor + ", baseDamage=" + baseDamage
+				+ ", experience=" + experience + ", sanctified=" + sanctified + "]";
+	}
+	
+	
+
+	// SETTERS Y GETTERS
+
+	public int getBaseDamage() {
+		return baseDamage;
+	}
+	
+	public boolean isSanctified() {
+		return sanctified;
+	}
+
+	// METODOS
+	public void takeDamage(int receivedDamage) {
+		this.armor = this.armor - receivedDamage;
+	}
+
+	public int getActualArmor() {
+		return this.armor;
+	}
+
+	public void resetArmor() {
+		this.armor = initialArmor;
+	}
+
+	public void setExperience(int n) {
+		this.experience = n;
+	}
+
+	public int getExperience() {
+		return experience;
+	}
+	
+	
+
+	
+	
+	
+	
+	
+	
 	
 	
 	
--- M3/src/defenseUnit/RocketLauncherTower.java
+++ M3/src/defenseUnit/RocketLauncherTower.java
@@ -7,50 +7,34 @@ class RocketLauncherTower extends DefenseUnit {
 	}
 	
 	public int attack() {
-		return 0;
-	}
-
-	public void takeDamage(int receivedDamage) {
-		
-	}
-
-	public int getActualArmor() {
-		return 0;
+		int damage_attack_does = getBaseDamage() + getBaseDamage() * (getExperience() * PLUS_ATTACK_UNIT_PER_EXPERIENCE_POINT) / 100;
+		if (isSanctified()) {
+			damage_attack_does += getBaseDamage() * PLUS_ATTACK_UNIT_SANCTIFIED / 100;
+		} 
+		return damage_attack_does;
 	}
 
 	public int getFoodCost() {
-		return 0;
+		return FOOD_COST_ROCKETLAUNCHERTOWER;
 	}
 
 	public int getWoodCost() {
-		return 0;
+		return WOOD_COST_ROCKETLAUNCHERTOWER;
 	}
 
 	public int getIronCost() {
-		return 0;
+		return IRON_COST_ROCKETLAUNCHERTOWER;
 	}
 
 	public int getManaCost() {
-		return 0;
+		return MANA_COST_ROCKETLAUNCHERTOWER;
 	}
 
 	public int getChanceGeneratingWaste() {
-		return 0;
+		return CHANCE_GENERATNG_WASTE_ROCKETLAUNCHERTOWER;
 	}
 
 	public int getChanceAttackAgain() {
-		return 0;
-	}
-
-	public void resetArmor() {
-		
-	}
-
-	public void setExperience(int n) {
-		
-	}
-
-	public int getExperience() {
-		return 0;
+		return CHANCE_ATTACK_AGAIN_ROCKETLAUNCHERTOWER;
 	}
 }
