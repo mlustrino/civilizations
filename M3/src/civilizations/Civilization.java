c024b900bcdbcc19d3bb147fb8e70be3cf4f84e0
unaiesvaz <unaiesvaz@gmail.com>
2026-05-17 19:17:54 +0200

    Clase Battle Funcional

--- M3/src/civilizations/Civilization.java
+++ M3/src/civilizations/Civilization.java
@@ -25,8 +25,8 @@ public class Civilization implements Variables {
     public static final int ARMY_LENGTH = 9;
     
 	// Technology
-	private int technologyDefense ;
-	private int technologyAttack ;
+	private int technologyDefense = 1 ;
+	private int technologyAttack = 1;
 	
 	// Recursos
 	private int wood;
@@ -254,9 +254,14 @@ public class Civilization implements Variables {
 		for (int i = 0; i < toAdd; i++) {
 
 			switch (idx_army) {
-				case IDX_ARMY_SWORDSMAN :
-					army[IDX_ARMY_SWORDSMAN].add(new Swordsman(technologyDefense,technologyAttack));
-					break;
+//				case IDX_ARMY_SWORDSMAN :
+//					army[IDX_ARMY_SWORDSMAN].add(new Swordsman(technologyDefense,technologyAttack));
+//					break;
+				case IDX_ARMY_SWORDSMAN: //BUG AQUI
+				    int armor = ARMOR_SWORDSMAN + (technologyDefense * PLUS_ARMOR_SWORDSMAN_BY_TECHNOLOGY) * ARMOR_SWORDSMAN / 100;
+				    int damage = BASE_DAMAGE_SWORDSMAN + (technologyAttack * PLUS_ATTACK_SWORDSMAN_BY_TECHNOLOGY) * BASE_DAMAGE_SWORDSMAN / 100;
+				    army[IDX_ARMY_SWORDSMAN].add(new Swordsman(armor, damage));
+				    break;
 	
 				case IDX_ARMY_SPEARMAN :
 					army[IDX_ARMY_SPEARMAN].add(new Spearman(technologyDefense,technologyAttack));
